b97c20320e77b8dbceecf49ec31d1fa8bc5bdda3
chavdajay <115387491+chavdajay@users.noreply.github.com>
2025-05-09 18:02:41 +0530

    Update Home.js

--- frontend/src/page/Home.js
+++ frontend/src/page/Home.js
@@ -1,9 +1,21 @@
-import React from 'react'
+// src/page/Home.jsx
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
-  return (
-    <div>home</div>
-  )
-}
+  const navigate = useNavigate();
 
-export default Home
+  const handleLogout = () => {
+    localStorage.removeItem('token');
+    navigate('/login');
+  };
+
+  return (
+    <div>
+      <h2>Welcome to Home Page</h2>
+      <button onClick={handleLogout}>Logout</button>
+    </div>
+  );
+};
+
+export default Home;
