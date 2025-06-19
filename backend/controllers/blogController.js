b558433e21fa68f1c2822b38d23c17b7f6f21904
chavdajay <115387491+chavdajay@users.noreply.github.com>
2025-06-19 09:58:59 +0530

    Update blogController.js

--- backend/controllers/blogController.js
+++ backend/controllers/blogController.js
@@ -3,11 +3,14 @@ const {handleSuccess, handleError} = require('../utils/responseHandler');
 
 exports.createUser = async(req, res)=>{
     try{
-        const newUser = new Blog(req.body);
+        const { title, description, date } = req.body;
+        const img = req.file ? req.file.filename : null;
+        
+        const newUser = new Blog({title, description, date, img});
         await newUser.save();
         return handleSuccess(res, "Create blog successfully", newUser);
     }catch(err){
-        console.error(err);
+         console.error(err);
          return handleError(res, "Internal server error", 500);
     }
 }
@@ -23,13 +26,17 @@ exports.fetchUsers = async(req, res)=>{
 
 exports.updateUsers = async(req, res)=>{
     try{
-        const user = await Blog.findByIdAndUpdate(req.params.id, req.body, {new:true});
+        const updateData = req.body;
+        if (req.file) updateData.image = req.file.filename;
+
+        const user = await Blog.findByIdAndUpdate(req.params.id, updateData, {new:true});
         if(!user) return handleError(res, "User not Found", 401);
         return handleSuccess(res, "BLog Updated Successfuly.", user);
     }catch(err){
         return handleError(res, "Internal server error", 500)
     }
 }
+
 exports.deleteUsers = async(req, res)=>{
     try{
         const user = await Blog.findByIdAndDelete(req.params.id);
