e2205f4ef54ded2e1f0d36c4a91dbedeaf65568c
chavdajay <115387491+chavdajay@users.noreply.github.com>
2025-06-19 10:00:18 +0530

    Update blogRoute.js

--- backend/routes/blogRoute.js
+++ backend/routes/blogRoute.js
@@ -1,12 +1,22 @@
 const express = require('express');
+const multer = require('multer');
 const router = express.Router();
 const blogController = require('../controllers/blogController');
 
-router.post('/create', blogController.createUser);
+
+// Image Storage Configuration
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => cb(null, 'uploads/'),
+  filename: (req, file, cb) => cb(null, Date.now() + '-' + file.originalname),
+});
+
+const upload = multer({ storage });
+
+router.post('/create',  upload.single('image'), blogController.createUser);
 
 router.get('/get', blogController.fetchUsers);
 
-router.put('/update/:id', blogController.updateUsers);
+router.put('/update/:id',  upload.single('image'), blogController.updateUsers);
 
 router.delete('/delete/:id', blogController.deleteUsers);
 
